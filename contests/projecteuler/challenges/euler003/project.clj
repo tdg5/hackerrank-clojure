54b76ddeee310e32a39f196fbe647804e8d27428
Danny Guinther <dannyguinther@gmail.com>
2016-10-31 10:43:21 -0400

    Add skeleton for euler003

---
+++ contests/projecteuler/challenges/euler003/project.clj
@@ -0,0 +1,7 @@
+(defproject euler003 "0.1.0"
+  :description "Clojure solution to https://www.hackerrank.com/contests/projecteuler/challenges/euler003"
+  :url "https://github.com/tdg5/hackerrank-clojure/contests/projecteuler/challenges/euler003/"
+  :main euler003.core
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
